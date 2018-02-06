11645ed6aa4f7f054e8bf7e8b0ec5ea38d6cb098
Trenton Hood <hoodtj@gmail.com>
2018-02-06 13:12:48 -0500

    Add files via upload
    Added comments and replaced print_r(fgetcsv($file)) with $info = fgetcsv($file).

--- SFP_PHP_TEST/src/Fibonacci.php
+++ SFP_PHP_TEST/src/Fibonacci.php
@@ -6,9 +6,9 @@ class Fibonacci
 	{
 $file = fopen(("C:\Users\hoodt\Desktop\SFP_PHP_TEST\assets\fibonacci.csv", "r");
 	while(! feof($file)){
-		print_r(fgetcsv($file));
-		for(i = 0, i < $file, $i += 9){
-		echo $file[$i] </br>;
+		$info = fgetcsv($file);
+		for(i = 0, i < $info, $i += 9){
+		echo $info[$i] </br>;
 		}
 	}
 fclose($file);
