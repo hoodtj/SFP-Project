f6b3ff1ef1f68406508c77e26336ff54d5870e7c
Trenton Hood <hoodtj@gmail.com>
2018-02-06 13:29:26 -0500

    Add files via upload
    Added some comments.

--- SFP_PHP_TEST/src/Fibonacci.php
+++ SFP_PHP_TEST/src/Fibonacci.php
@@ -7,8 +7,8 @@ class Fibonacci
 $file = fopen(("C:\Users\hoodt\Desktop\SFP_PHP_TEST\assets\fibonacci.csv", "r");
 	while(! feof($file)){
 		$info = fgetcsv($file);
-		for(i = 0, i < $info, $i += 9){
-		echo $info[$i] </br>;
+		for(i = 0, i < $info, $i += 9){		//For loop which runs till argument is false
+		echo $info[$i] </br>;				//Displays each result of for loop
 		}
 	}
 fclose($file);
